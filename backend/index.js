e80fa7cf3c026294ea38b9f305686f3bded61914
YaShRaJ0P <helloiamyashraj0405@gmail.com>
2024-06-13 12:11:16 +0530

    minor improvement

--- backend/index.js
+++ backend/index.js
@@ -14,15 +14,6 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Middleware for parsing JSON request body
 
-// CORS headers manually
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 // Routes
 app.use("/books", booksRoute);
 app.get("/", (req, res) => {
